bae2b83c3d829256425cf6104644b260ca4a5ee4
Shubham <shubham.22110855@viit.ac.in>
2026-07-05 11:24:10 +0530

    chore: add dockdr-compose for production security and caching

--- app/main.py
+++ app/main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
+import os
 
 app = FastAPI()
+
+REDIS_URL = os.getenv("REDIS_URL")
+DATABASE_URL = os.getenv("DATABASE_URL")
+QDRANT_URL = os.getenv("QDRANT_URL")
 
 
 @app.get("/")
 def home():
     return {
-        "message": "AIFlow CI/CD Pipeline Working!"
+        "status": "AIFlow running",
+        "services": {
+            "redis": REDIS_URL,
+            "postgres": DATABASE_URL,
+            "qdrant": QDRANT_URL
+        }
     }
