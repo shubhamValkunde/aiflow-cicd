2c60a9dd2eb167f36b3bbaaa426b2ee8c5cf74b1
Shubham <shubham.22110855@viit.ac.in>
2026-07-05 11:27:29 +0530

    chore: add dockdr-compose for

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,13 +1,16 @@
 from fastapi.testclient import TestClient
-
 from app.main import app
 
 client = TestClient(app)
 
-
 def test_home():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {
-        "message": "AIFlow CI/CD Pipeline Working!"
-    }
+
+    data = response.json()
+
+    assert data["status"] == "AIFlow running"
+    assert "services" in data
+    assert "redis" in data["services"]
+    assert "postgres" in data["services"]
+    assert "qdrant" in data["services"]
